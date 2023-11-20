fc8a4e69e5881907bfb8d279623b4e0ab3a291e1
Boyia <1353999534@qq.com>
2023-11-20 15:32:43 +0800

    day 3

---
+++ hackerrank/diagonalDifference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    leftsum = 0
+    rightsum = 0
+    for i in range(len(arr)):
+        leftsum+= arr[i][i]
+        rightsum+= arr[i][-1-i]
+    return abs(leftsum - rightsum)
+
+    
+        
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    print(str(result) + '\n')
+
